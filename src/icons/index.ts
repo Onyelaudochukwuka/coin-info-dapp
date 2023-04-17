dc389d900b29824eed7123d416d5241a38efdc80
Udoka <udochukwukaonyela@gmail.com>
2023-04-17 21:57:12 +0100

    feat: exported icvons

---
+++ src/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as CloseIcon } from './CloseIcon.icon';
+export { default as GithubIcon } from './GithubIcon.icon';
+export { default as TwitterIcon } from './TwitterIcon.icon';
+export { default as LinkedInIcon } from './LinkedInIcon.icon';
+export { default as Loading } from './Loading.icon';
